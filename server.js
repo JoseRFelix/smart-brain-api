95819cca1a1ec0d53d03b168078f44d479c75ba8
jfelix61 <rtfire456@hotmail.com>
2018-06-19 01:56:26 -0400

    change response /

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('it is working') });
 app.post('/signin', signIn.handleSignIn(db, bcrypt));
 app.post('/register',  register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
